3fd264af1cee70281ddc58f9f77bc96548485629
Olivier <codol.olivier@gmail.com>
2021-12-23 11:50:53 -0500

    Update content

--- example_plots.m
+++ example_plots.m
@@ -12,7 +12,8 @@ cc = [0.2422    0.1504    0.6603]; % color is purple
 a = linspace(.1,1,ng); % let's have fun with the alpha parameter here
 
 % Initialise figure
-figure(1); clf;
+hf(1) = figure(1); clf;
+hf(1).Position = [0 0 1150 1000];
 [nrow,ncol] = deal(4,5);
 for k = 1:(nrow*ncol)
     ha(k)=subplot(nrow,ncol,k);
@@ -28,7 +29,7 @@ OPTIONS = {'color',cc,'cluster',CL,'alpha',a,'ycut',1};
 % Create barplots. This illustrate different types of dot jittering
 barreplot(X,'parent',ha(1),OPTIONS{:},'dots','align');
 barreplot(X,'parent',ha(2),OPTIONS{:},'dots','deport');
-barreplot(X,'parent',ha(3),OPTIONS{:},'dots','jitter'); title('barreplot');
+barreplot(X,'parent',ha(3),OPTIONS{:},'dots','jitter'); title('Barreplot');
 barreplot(X,'parent',ha(4),OPTIONS{:},'dots','silo');
 barreplot(X,'parent',ha(5),OPTIONS{:},'dots','none');
 
@@ -42,7 +43,7 @@ OPTIONS{2} = parula(ng); % change colormap because this is prettier here
 % very practical to swap between functions. 
 dotplot(X,'parent',ha(6),OPTIONS{:},'dots','align');
 dotplot(X,'parent',ha(7),OPTIONS{:},'dots','deport');
-dotplot(X,'parent',ha(8),OPTIONS{:},'dots','jitter'); title('dotplot');
+dotplot(X,'parent',ha(8),OPTIONS{:},'dots','jitter'); title('Dotplot');
 dotplot(X,'parent',ha(9),OPTIONS{:},'dots','silo');
 dotplot(X,'parent',ha(10),OPTIONS{:},'dots','none');
 
@@ -54,7 +55,7 @@ OPTIONS{2} = [0.2422    0.1504    0.6603]; % color is back to purple
 
 boiteplot(X,'parent',ha(11),OPTIONS{:},'dots','align');
 boiteplot(X,'parent',ha(12),OPTIONS{:},'dots','deport');
-boiteplot(X,'parent',ha(13),OPTIONS{:},'dots','jitter'); title('boiteplot');
+boiteplot(X,'parent',ha(13),OPTIONS{:},'dots','jitter'); title('Boiteplot');
 boiteplot(X,'parent',ha(14),OPTIONS{:},'dots','silo');
 boiteplot(X,'parent',ha(15),OPTIONS{:},'dots','none');
 
@@ -65,17 +66,21 @@ boiteplot(X,'parent',ha(15),OPTIONS{:},'dots','none');
 % The default is ['marker','-'] though.
 OPTIONS = [OPTIONS,'marker','o'];
 
-rainplot(X,'parent',ha(16),OPTIONS{:},'dots','align');
-rainplot(X,'parent',ha(17),OPTIONS{:},'dots','deport');
-rainplot(X,'parent',ha(18),OPTIONS{:},'dots','jitter'); title('rainplot');
-rainplot(X,'parent',ha(19),OPTIONS{:},'dots','silo');
-rainplot(X,'parent',ha(20),OPTIONS{:},'dots','none');
+rainplot(X,'parent',ha(16),OPTIONS{:},'dots','align'); ha(16).XAxis.Label.String = 'align';
+rainplot(X,'parent',ha(17),OPTIONS{:},'dots','deport'); ha(17).XAxis.Label.String = 'deport';
+rainplot(X,'parent',ha(18),OPTIONS{:},'dots','jitter'); ha(18).XAxis.Label.String = 'jitter'; title('Rainplot');
+rainplot(X,'parent',ha(19),OPTIONS{:},'dots','silo'); ha(19).XAxis.Label.String = 'silo';
+rainplot(X,'parent',ha(20),OPTIONS{:},'dots','none'); ha(20).XAxis.Label.String = 'none';
 
+%% CREATE ANNOTATIONS
+
+annotation(hf(1),'textbox',[.04 .07 .15 .03],'String','dot layout','EdgeColor','none','FontSize',9)
+annotation(hf(1),'arrow','Position',[.11 .085 .035 0],'LineWidth',0.5)
 
 %% GAUGEPLOT
 
 % Initialise figure
-hf = figure(2); clf
+hf(2) = figure(2); clf
 
 % Fake data
 [ncat,ng] = deal(10,8);
@@ -85,7 +90,7 @@ ha = axes('Position',[.07 .07 .3 .3]);
 OPTIONS = {'color',cool(ncat),'cluster',[1,1,1,2,3,3,4,4],'alpha',1};
 
 gaugeplot(X,'parent',ha,OPTIONS{:});%,'cluster',CL,'alpha',a,'dots','align','ycut',1,'marker','o');
-title('gaugeplot')
+title('Gaugeplot')
 
 
 
@@ -96,11 +101,11 @@ title('gaugeplot')
 X = randn(nsample,ng) + (1:ng)*2;
 Y = randn(nsample,ng) - (1:ng)*0.5;
 
-OPTIONS = {'color',jet(ng),'parent',hf,'nbins',10,'zeroline','--'};
+OPTIONS = {'color',jet(ng),'parent',hf(2),'nbins',10,'zeroline','--'};
 
 scatterdist(X,Y,'hd','distribution','position',[.5   0 .5 .5],OPTIONS{:});
 scatterdist(X,Y,'hd','histogram'   ,'position',[.5  .46 .5 .5],OPTIONS{:});
-title('scatterdist')
+title('Scatterdist')
 
 
 %% DISTLINE
@@ -113,16 +118,17 @@ X = mat2cell(randn(nsample,ng) + 7*randn(1,ng),nsample,ones(1,ng));
 % Distributions with empty patches
 ha = axes('Position',[.07 .5 .4 .15]);
 distline(X,'parent',ha,'alpha',0,'color',summer(ng));
+title('Distline (empty patches)')
 
 % Distributions with filled patches
 ha = axes('Position',[.07 .75 .4 .15]);
 distline(X,'parent',ha,'alpha',.7,'color',summer(ng));
-title('distline')
+title('Distline')
 
 %% LEGENDS
 
 LABELS = {'one','two','three'};
-legendline(hf,LABELS, summer(3),'position',[.36 .55 .18 .07],'fliplr',1);
-legendbox (hf,LABELS, cool(3)  ,'position',[.38 .1 .18 .07]);
+legendline(hf(2),LABELS, summer(3),'position',[.36 .55 .18 .07],'fliplr',1);
+legendbox (hf(2),LABELS, cool(3)  ,'position',[.38 .1 .18 .07]);
 
 
